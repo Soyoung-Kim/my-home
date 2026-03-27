cda5e1af451704e3cb7314cf973935eaf75bc0d0
soyoung <syoung@nongshim.co.kr>
2026-03-27 16:37:43 +0900

    경로 수정.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import { AppRoutes } from './router';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="{process.env.PUBLIC_URL}">
       <AppRoutes />
     </BrowserRouter>
   );
